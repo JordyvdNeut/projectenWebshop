6657021e77461668c09f33bf0c4f35b0b507ca67
JordyvdNeut <jordyneut@hotmail.com>
2019-03-04 14:51:45 +0100

    Fixed problems

---
+++ webshopMultiversum/webshop/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Multiversum</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <link rel="shotcut icon" type="image/png" href="images/logo.png" />
+</head>
+
+<body>
+    <header>
+        <?php include 'navigation/header/header.php'; ?>
+    </header>
+    <section>
+        joe joeh!
+    </section>
+    <footer>
+        <?php include 'navigation/footer/footer.php'; ?>
+    </footer>
+</body>
+
+</html> 

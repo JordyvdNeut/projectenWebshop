64914c160bfb55c8a981369f93fe853c5c562289
JordyvdNeut <47422876+JordyvdNeut@users.noreply.github.com>
2019-04-08 19:54:28 +0200

    add a icon

--- webshopMultiversum/adminConsole/view/producten/removeProduct.php
+++ webshopMultiversum/adminConsole/view/producten/removeProduct.php
@@ -1,2 +1,8 @@
-<h2 color="red">Weet u zeker dat u dit product wilt verwijderen</h2>
-<button class="btn"></button>
+<section>
+  <div class="thumbnail">
+    <h2 align="center" color="red">
+      Weet u zeker dat u dit product wilt verwijderen
+    </h2>
+    <button class="btn"><i class="fa fa-close"></i></button>
+  </div>
+</section>

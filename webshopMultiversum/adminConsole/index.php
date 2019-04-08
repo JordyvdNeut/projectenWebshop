d30ec4797b49bff8929ede8169557f23bc16f77c
JordyvdNeut <jordyneut@hotmail.com>
2019-04-08 19:50:41 +0200

    w3schools icons toegevoegd

--- webshopMultiversum/adminConsole/index.php
+++ webshopMultiversum/adminConsole/index.php
@@ -8,6 +8,8 @@
 
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
@@ -27,14 +29,14 @@
   //   $authController->handleRequest();
   // }
   // if ($authController->authRequest() == true) {
-    // Header
-    include 'view/navigation/navBar/navBar.php';
+  // Header
+  include 'view/navigation/navBar/navBar.php';
 
-    // Content
-    $productsController->handleRequest();
+  // Content
+  $productsController->handleRequest();
 
-    // Footer
-    include 'view/navigation/footer/footer.php';
+  // Footer
+  include 'view/navigation/footer/footer.php';
   // }
   ?>
 </body>

e53cb45fc22742b5362cde7451ef41641aa2567e
JordyvdNeut <jordyneut@hotmail.com>
2019-04-11 23:11:24 +0200

    removed populair products

--- webshopMultiversum/webshop/view/home/home.php
+++ webshopMultiversum/webshop/view/home/home.php
@@ -1,23 +1,40 @@
 <section>
-    <div class="container" id="aanbiedingen">
-        <h2>Aanbiedingen</h2>
-        <p>text</p>
+    <div class="thumbnail" id="aanbiedingen">
+        <h2>Aanbiedingen:</h2>
+        <?php
+            echo "Sorry de producten kunnen niet geladen worden";
+        // if ($products) {
+            // while ($row = $products->fetch(PDO::FETCH_ASSOC)) {
+            //     $row['Prijs'] = str_replace('.', ',', $row['Prijs']);
+            //     $row = preg_replace('/^$/', '<i>Geen gevens kunnen vinden</i>', $row);
+
+            //     $html .= "<div class='col-md-3 productMarg thumbnail' href='index?op=details'>";
+            //     $html .=  "<h3>" . $row['Naam'] . "</h3>";
+            //     // $html .=  "<img src='" . $row['Img'] . "' class='img-responsive' alt='fix img' />";
+            //     $html .=  "<div class='caption'>";
+            //     $html .=  "<table>";
+            //     $html .=  "<tr>";
+            //     $html .=  "<th>" . "Prijs" . "</th>";
+            //     $html .=  "<td align='right'>€ " . $row['Prijs'] . "</td>";
+            //     $html .=  "</tr>";
+            //     $html .=  "<tr>";
+            //     $html .=  "<th>" . "AanbiedingPrijs" . "</th>";
+            //     $html .=  "<td align='right'>€ " . $row['AanbiedingPrijs'] . "</td>";
+            //     $html .=  "</tr>";
+            //     $html .=  "<tr>";
+            //     $html .=  "<th>" . "Merk" . "</th>";
+            //     $html .=  "<td align='right'>" . $row['Merk'] . "</td>";
+            //     $html .=  "</tr>";
+            //     $html .=  "</table>";
+            //     $html .=  "</div>";
+            //     $html .=  "<a href='index?op=details&id=" . $row['ProductsID'] . "'><button class='btn'>Details</button></a>";
+            //     $html .=  "<a href='index?op=winkelwagen'><button class='btn'>Bestellen</button></a>";
+            //     $html .=  "</div>";
+            // }
+            // echo $html;
+        // } else {
+            // echo "Sorry de producten kunnen niet geladen worden";
+        // };
+        ?>
     </div>
-    <div class="poProducts container" id="producten">
-        <h2>Popularen producten</h2>
-        <div class="row">
-            <div class="col-sm-3">
-                text
-            </div>
-            <div class="col-sm-3">
-                text
-            </div>
-            <div class="col-sm-3">
-                text
-            </div>
-            <div class="col-sm-3">
-                text
-            </div>
-        </div>
-    </div>
-</section> 
+</section>
